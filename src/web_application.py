ca05906bd56b6436f03e8d6e43dfba56f7947b0c
CsPaulo <cspaulorafael@gmail.com>
2023-10-03 15:13:25 -0300

    improving upload

--- src/web_application.py
+++ src/web_application.py
@@ -1,73 +1,84 @@
-import streamlit.components.v1 as components
-import lime.lime_tabular
 import streamlit as st
+import cv2
 import mahotas
 import pickle
-import pandas as pd
 import numpy as np
-import cv2
-
+import pandas as pd
+import pandas
+from PIL import Image
+import lime
+import lime.lime_tabular
 from sklearn.model_selection import train_test_split
+import streamlit.components.v1 as components
 
-# Função que carrega o modelo
+# carrega o modelo
 def get_model():
     return pickle.load(open('C:/Users/cspau/Desktop/coisas do pc/Aprendendo Python/GitHub/leaf-diagnostic/etc/best_random_forest_model.dat', 'rb'))
 
-# Função para converter dados da imagem
+# converter dados da imagem
 def convert_byteio_image(string):
     array = np.frombuffer(string, np.uint8)
     image = cv2.imdecode(array, flags=1)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
-# Função para interpretar e exibir as previsões
-def interpret_and_display_predictions(model, features, X_train):
-    explainer = lime.lime_tabular.LimeTabularExplainer(X_train.values, feature_names=X_train.columns, class_names=['Saudável', 'moscas-das-galhas'],
-                                                       feature_selection='lasso_path', discretize_continuous=True)
-    exp = explainer.explain_instance(features.reshape(14,), model.predict_proba, num_features=14)
-    return exp.as_html(predict_proba=False)
+# interpretar e exibir as previsões com interpretação usando Lime
+def interpret_and_display_predictions(model, image, features):
+    features = mahotas.features.haralick(image, compute_14th_feature = True, return_mean = True).reshape(1, 14)
 
-# Título web
+    df = pandas.read_csv('C:/Users/cspau/Desktop/coisas do pc/Aprendendo Python/GitHub/leaf-diagnostic/etc/features.csv', delimiter = ';')
+
+    X = df.drop('Label', axis = 1)
+    y = df['Label']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+    explainer = lime.lime_tabular.LimeTabularExplainer(X_train.values, feature_names = X.columns, class_names = ['Saudável', 'Moscas das Galhas'], 
+                                                                feature_selection = 'lasso_path', discretize_continuous = True)
+        
+    exp = explainer.explain_instance(features.reshape(14,), model.predict_proba, num_features = 14)
+    
+    st.image(image, caption="Imagem", use_column_width=True)
+    st.markdown("<h3 style='text-align: center; color: black;'>Interpretação</h3>", unsafe_allow_html=True)
+
+    
+    components.html((exp.as_html(predict_proba = False)), height=500)
+
+    
+# título web
 st.markdown("<h1 style='text-align: center; color: black;'>Aplicação web para diagnosticar moscas-das-galhas nas folhas </h1>", unsafe_allow_html=True)
 
-# Barra lateral para upload de imagens
+# barra lateral para upload de imagens
 st.sidebar.title('Configurações')
-uploaded_image = st.sidebar.file_uploader("Escolha a imagem", type='jpg', accept_multiple_files=False)
-
+uploaded_images = st.sidebar.file_uploader("Escolha até 10 imagens", type=['jpg', 'jpeg'], accept_multiple_files=True, key="upload_images")
 
 model = get_model()
 
+if uploaded_images:
+    images_and_features = []
+    
+    for uploaded_image in uploaded_images:
+        bytes_data = uploaded_image.getvalue()
+        image = convert_byteio_image(bytes_data)
 
-if uploaded_image is not None:
-    bytes_data = uploaded_image.getvalue()
-    image = convert_byteio_image(bytes_data)
+        if image.shape != (256, 256):
+            image = cv2.resize(image, (256, 256))
 
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    if (image.shape != (256, 256)):
-        image = cv2.resize(image, (256, 256))
+        # extrair características da imagem
+        features = mahotas.features.haralick(gray_image, compute_14th_feature=True, return_mean=True).reshape(14,)
+        
+        images_and_features.append((image, features))
 
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # exibir imagens e previsões com interpretação
+    st.markdown("<h3 style='text-align: center; color: black;'>Imagens e Previsões</h3>", unsafe_allow_html=True)
 
-    # Extrair características da imagem
-    features = mahotas.features.haralick(gray_image, compute_14th_feature=True, return_mean=True).reshape(1, 14)
+    for img, features in images_and_features:
+        
+        prediction_probs = model.predict_proba([features])
+        prediction = "Folha afetada pelas moscas-das-galhas com {:.2%} de certeza".format(prediction_probs[0][1]) if prediction_probs[0][1] > prediction_probs[0][0] else "Folha normal com {:.2%} de certeza".format(prediction_probs[0][0])
 
-    # Fazer previsões
-    prediction_probs = model.predict_proba(features)
-    prediction = "Folha afetada pelas moscas-das-galhas com {:.2%} de certeza".format(prediction_probs[0][1]) if prediction_probs[0][1] > prediction_probs[0][0] else "Folha normal com {:.2%} de certeza".format(prediction_probs[0][0])
-
-    # Exibir imagem e previsões
-    st.markdown("<h3 style='text-align: center; color: black;'>Imagem</h3>", unsafe_allow_html=True)
-    col1, col2, col3 = st.columns([0.2, 5, 0.2])
-    col2.image(gray_image, use_column_width=True)
-    st.markdown("<h4 style='text-align: center; color: black;'>" + prediction + "</h4>", unsafe_allow_html=True)
-
-    # Botão para exibir interpretação
-if st.sidebar.button("Exibir interpretação"):
-    st.markdown("<h3 style='text-align: center; color: black;'>Interpretação</h3>", unsafe_allow_html=True)
-    with st.spinner('Calculando...'):
-        # Carregar dados e treinar o modelo
-        df = pd.read_csv('C:/Users/cspau/Desktop/coisas do pc/Aprendendo Python/GitHub/leaf-diagnostic/etc/features.csv', delimiter=';')
-        X_train, X_test, _, _ = train_test_split(df.drop('Label', axis=1), df['Label'], test_size=0.2, random_state=42)
-        # Interpretar e exibir as previsões
-        html_exp = interpret_and_display_predictions(model, features, X_train)
-        components.html(html_exp, height=800)
+        st.markdown(f"<h4 style='text-align: center; color: black;'>{prediction}</h4>", unsafe_allow_html=True)
+        
+        interpret_and_display_predictions(model, img, features)
